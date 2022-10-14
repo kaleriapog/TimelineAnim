1abab226fbfffa2fb2def155894345470dadaa35
kaleriapog <pogrelova.kaleria@gmail.com>
2022-10-14 17:13:40 +0300

    add

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let timelineSection = new TimelineMax();
     let timelineSection2 = new TimelineMax();
     let allInput = document.querySelectorAll('.interactive-name-class .input')
+    let allInputForCard = document.querySelectorAll('.interactive-name-class2 .input')
 
     timelineSection
         .fromTo(['.item-0'], {x: '0'}, {x: '50vw', duration: 1, delay: 0, ease: Linear.easeNone})
@@ -64,7 +65,6 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
-
     // for  name-class2
     timelineSection2
             .fromTo(['.card-0'], {}, {y: '-10vh', transform: 'scale(0.8)', duration: 1, delay: 0, ease: Linear.easeNone})
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
             .fromTo(['.card-2'], {}, {y: '-10vh', transform: 'scale(0.8)', duration: 1, delay: 0, ease: Linear.easeNone})
             .fromTo(['.card-3'], {y: '100vh'}, {y: '0', duration: 1, delay: 0, ease: Linear.easeNone}, '<');
 
-        let sceneAnim2 = new ScrollMagic.Scene({ triggerElement: '.name-class2',  triggerHook: 'onLeave', duration: '1000', tweenChanges: true }) //в duration (продолжительность) может быть любое число в пикселях, но само слово "рх" не нужно, просто укажити число. ну или укажите "100%" - будет равно высоте экрана
+        let sceneAnim2 = new ScrollMagic.Scene({ triggerElement: '.name-class2',  triggerHook: 'onLeave', duration: '2000', tweenChanges: true }) //в duration (продолжительность) может быть любое число в пикселях, но само слово "рх" не нужно, просто укажити число. ну или укажите "100%" - будет равно высоте экрана
             .setPin('.name-class2')
             .setTween(timelineSection2)
             .on("progress", function (e) {
@@ -83,4 +83,37 @@ document.addEventListener('DOMContentLoaded', () => {
             })
             .addIndicators({name: "мой second блок"})
             .addTo(controller);
+
+
+    allInputForCard.forEach((input) => {
+        input.addEventListener('input', () => {
+
+            let cardValueDefault = document.querySelector('.card-value-default').value
+            let cardValueEnd = document.querySelector('.card-value-end').value
+            let cardValueScale = document.querySelector('.card-value-scale').value
+            let cardValuePos = document.querySelector('.card-value-pos').value
+            let cardValueDuration = document.querySelector('.card-value-duration').value
+
+            sceneAnim2.duration(cardValueDuration)
+
+            let timeline = new TimelineMax();
+            let tweencard0 = TweenMax.fromTo(['.card-0'], {}, {y: `${cardValuePos}`, transform: `scale(${cardValueScale})`, duration: 1, delay: 0, ease: Linear.easeNone})
+            let tweencard1 = TweenMax.fromTo(['.card-1'], {y: `${cardValueDefault}`}, {y: `${cardValueEnd}`, duration: 1, delay: 0, ease: Linear.easeNone}, '<')
+            let tweencard2 = TweenMax.fromTo(['.card-1'], {}, {y: `${cardValuePos}`, transform: `scale(${cardValueScale})`, duration: 1, delay: 0, ease: Linear.easeNone})
+            let tweencard3 = TweenMax.fromTo(['.card-2'], {y: `${cardValueDefault}`}, {y: `${cardValueEnd}`, duration: 1, delay: 0, ease: Linear.easeNone}, '<')
+            let tweencard4 = TweenMax.fromTo(['.card-2'], {}, {y: `${cardValuePos}`, transform: `scale(${cardValueScale})`, duration: 1, delay: 0, ease: Linear.easeNone})
+            let tweencard5 = TweenMax.fromTo(['.card-3'], {y: `${cardValueDefault}`}, {y: `${cardValueEnd}`, duration: 1, delay: 0, ease: Linear.easeNone}, '<');
+
+            timeline
+                .add(tweencard0)
+                .add(tweencard1)
+                .add(tweencard2)
+                .add(tweencard3)
+                .add(tweencard4)
+                .add(tweencard5);
+
+            sceneAnim2.setTween(timeline);
+
+        })
+    })
 })
